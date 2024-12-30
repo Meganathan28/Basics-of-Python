b230bd62cd5aba3d70c7c7f3dc66e96c743d511a
Meganathan Saravanan <meganathansaravanan4615@gmail.com>
2024-12-30 15:40:46 +0530

    String inbuilt functions

--- String Functions.py
+++ String Functions.py
@@ -2,6 +2,25 @@
 # Here, Some necessary methods in String
 print("Learning some string inbuilt Functions:")
 # For Finding Length of the String 
-name = "Max"
+name = "max Coder"
+course = "ComputerScience"
+year = "3"
 print(len(name)) # Length of the string using len(string_name)
-# 
+# Find the Index of the Character 
+print(name.find("x")) #Finds whether the character is present
+#Capitalize the string
+print(name.capitalize())
+#UpperCase all the string
+print(name.upper())
+#Lowercase all the string
+print(name.lower())
+#Digit or Not
+print(year.isdigit()) #True or False
+#Alphabets or Not
+print(course.isalpha()) #same as like as isdigit() True, False
+#Count 
+print(course.count('e'))
+#Replace
+print(year.replace("3","3 and half"))
+
+
